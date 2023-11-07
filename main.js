bcd98b95da22c96fbd414b6af7c13ad4bfd11f00
Matheus Viscki <matheusviscki@gmail.com>
2023-11-06 21:06:19 -0300

    corrigido tamanho dos contianer

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-// lucide.createIcons();
+document.addEventListener('DOMContentLoaded', function () {
+  const optionContainers = document.querySelectorAll('.option-container');
+  const buyButtons = document.querySelectorAll('.buy-button');
+
+  optionContainers.forEach((container, index) => {
+    container.addEventListener('click', function () {
+      const selectedRadio = container.querySelector('input[type="radio"]:checked');
+
+      if (selectedRadio) {
+        const paymentLink = selectedRadio.value;
+        buyButtons[index].setAttribute('href', paymentLink);
+      }
+    });
+  });
+
+  buyButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      event.preventDefault(); // Impede o comportamento padrão de navegação
+      const paymentLink = button.getAttribute('href');
+
+      if (paymentLink) {
+        window.open(paymentLink, '_blank');; // Redireciona para o link de pagamento
+      }
+    });
+  });
+});
